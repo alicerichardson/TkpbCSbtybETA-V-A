97e202791a161f9de9d0b7254a4d389755d0083d
Richardson <RICHARDSON1@science.regn.net>
2019-06-19 11:45:15 -0400

    added component skeleton pages

---
+++ tkpbcsbtybeta-v-a/src/Components/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './NavBar';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h3>TkpbCSbtybETA@V+A</h3>
+            <div>
+                <NavBar />
+            </div>
+        </div>
+    );
+}
+export default HomePage;
