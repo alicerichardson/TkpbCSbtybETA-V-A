7187033e368d645e001915440d0d4ac6811f05a6
Bella Jr <BELLAG@science.regn.net>
2019-06-20 15:04:34 -0400

    Updated SignUp form

--- tkpbcsbtybeta-v-a/src/Components/SignUp.jsx
+++ tkpbcsbtybeta-v-a/src/Components/SignUp.jsx
@@ -1,6 +1,54 @@
-import React from 'react';
-import './css/movie.css';
+import React, { useState } from 'react'
+import './css/movie.css'
 
+const SignUp = () => {
+
+    const [FirstName, setFirstName] = useState('')
+    const [LastName, setLastName] = useState('')
+    const [Email, setEmail] = useState('')
+    const [Birthday, setBirthday] = useState('')
+    
+    const handleFirstNameChange = (event) => {
+        setFirstName([event.target.value])
+    }
+    const handleLastNameChange = (event) => {
+        setLastName([event.target.value])
+    }
+    const handleEmailChange = (event) => {
+        setEmail([event.target.value])
+    }
+    const handleBirthdayChange = (event) => {
+        setBirthday([event.target.value])
+    }
+    const handleSubmit = (event) => {
+        alert(`${FirstName} ${LastName} ${Email} ${Birthday}`)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Firstname: </label>
+                <input type="text" name='FirstName' value={FirstName} onChange={handleFirstNameChange}></input>
+            </div>
+            <div>
+                <label>Lastname: </label>
+                <input type="text" name='LastName' value={LastName} onChange={handleLastNameChange}></input>
+            </div>
+            <div>
+                <label>Email: </label>
+                <input type="text" name='Email' value={Email} placeholder="user@domain.com" onChange={handleEmailChange}></input>
+            </div>
+            <div>
+                <label>Birthday: </label>
+                <input type="date" name='Birthday' value={Birthday} onChange={handleBirthdayChange}></input>
+            </div>
+                <button type='submit'>Sign Up!</button>
+        </form>
+    )
+}
+export default SignUp
+
+/*
 const SignUp = () => {
     return (
         <div>
@@ -17,4 +65,31 @@ const SignUp = () => {
         </div>
     );
 }
-export default SignUp;
+
+
+
+const SignUp = (props) => {
+    const [FirstName, setFirstName] = useState('')
+    const [LastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [Birthday, setBirthday] = useState('')
+
+    const handleFirstName = (event) => {
+        setFirstName([event.target.value])
+    }
+
+    const handleLastName = (event) => {
+        setLastName([event.target.value])
+    }
+
+    const handleEmail = (event) => {
+        setEmail([event.target.value])
+    }
+
+    const handleBirthday = (event) => {
+        setBirthday([event.target.value])
+    }
+
+
+}
+*/
