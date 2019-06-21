f8e967dda0a44f187855d67c8ab9a97f0259fa25
Ebling <EBLINGE@science.regn.net>
2019-06-21 16:03:57 -0400

    Style changes

--- tkpbcsbtybeta-v-a/src/Components/Movie.jsx
+++ tkpbcsbtybeta-v-a/src/Components/Movie.jsx
@@ -7,21 +7,19 @@ const Movie = ({movie}) => {
     return (
         <div class="movie">
             <h1>{movie.name}</h1>
-            <div class=" col leftleft">
-                <img src={movieImg}></img>
-            </div>
             <div class="row">
+                <div class="col leftleft">
+                    <img src={movieImg}></img>
+                </div>
                 <div class="col left">
                     <h6>{movie.rating}</h6>
                     <h6>{movie.runtime}</h6>
                     <h6 class="info">More Info</h6>
                 </div>
                 <div class="col center">
-                    <ul>
-                        {movie.showtimes.map((value, index) => {return <Link to={{pathname:"/purchase", state:{time: value} }}><li class="showtimes" key={index}>{value}</li></Link>})}
-                    </ul>
+                    {movie.showtimes.map((value, index) => {return <Link to={{pathname:"/purchase", state:{time: value} }}><li class="showtimes" key={index}>{value}</li></Link>})}
                 </div>
-                <div>
+                <div class="col right">
                     <Link to={{ pathname:"/purchase", state:{name: movie.name, rating: movie.rating, runtime: movie.runtime} }}><button class ="purchase">Purchase</button></Link>
                 </div>
             </div>
