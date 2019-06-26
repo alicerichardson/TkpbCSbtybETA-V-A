c98708e9a0ef737ff17dcb6cfbb884ade45136a0
Bella Jr <BELLAG@science.regn.net>
2019-06-26 11:09:19 -0400

    Concessions added

--- tkpbcsbtybeta-v-a/src/Components/Concession.jsx
+++ tkpbcsbtybeta-v-a/src/Components/Concession.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import NavBar from './NavBar';
+import popcorn from '../popcorn.PNG';
+import soda from '../soda.png';
+import candy from '../candy.jpg';
+import concessionStand from '../ConcessionStand.jpg';
 
 const Concession = () => { 
     return (
         <div>
             <NavBar/> 
-            <h1>Popcorn</h1>
+            <h1>Concession Stand</h1>
+            <img class="concessionStand" src={concessionStand} alt="Theater Concession Stand"></img>
+            <table align="center">
+                <tr>
+                    <th>Item</th>
+                    <th>Small</th>
+                    <th>Medium</th>
+                    <th>Large</th>
+                </tr>
+                <tr>
+                    <th><img class="concession" src={popcorn} alt="Popcorn"></img></th>
+                    <th>$3.99</th>
+                    <th>$5.99</th>
+                    <th>$6.99</th>
+                </tr>
+                <tr>
+                    <th><img class="concession" src={soda} alt="Soda"></img></th>
+                    <th>$3.99</th>
+                    <th>$4.99</th>
+                    <th>$5.99</th>
+                </tr>
+                    <th><img class="concession" src={candy} alt="Candy"></img></th>
+                    <th colspan="3">$2.99</th>
+            </table>
         </div>
     );
 }
