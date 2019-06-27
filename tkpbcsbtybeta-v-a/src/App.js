38b665cf7027c1959c9b2499ca7cc3ffb1b2b88c
Bella Jr <BELLAG@science.regn.net>
2019-06-27 10:48:34 -0400

    Update Accessibility

--- tkpbcsbtybeta-v-a/src/App.js
+++ tkpbcsbtybeta-v-a/src/App.js
@@ -8,7 +8,7 @@ import Purchase from './Components/Purchase';
 import SignIn from './Components/SignIn';
 import TicketConfirmation from './Components/TicketConfirmation';
 import Concession from './Components/Concession';
-import Accessability from './Components/Accessability';
+import Accessibility from './Components/Accessibility';
 
 function App() {
   const routing = (
@@ -17,7 +17,7 @@ function App() {
         <Route path="/" component={HomePage} exact/>
         <Route path="/signup" component={SignUp}/>
         <Route path="/concessions" component={Concession}/>
-        <Route path="/accessability" component={Accessability}/>
+        <Route path="/accessbility" component={Accessibility}/>
         <Route path="/contact" component={Info}/>
         <Route path="/purchase" component={Purchase}/>
         <Route path="/signin" component={SignIn}/>
---
+++ tkpbcsbtybeta-v-a/src/Components/Accessibility.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import NavBar from './NavBar';
+import './css/homepage.css';
+
+const Accessibility = () => { 
+    return (
+        <div>
+        <NavBar/> 
+        <h1>Accessibility Technologies</h1>
+        <h4>
+            Movies are best enjoyed by everyone<br />
+            At TkpbCSbtybETA@V+A, our theater offers the following assistive technologies for our movie-goers:
+        </h4>
+        <h2>Wheelchair</h2>
+        <h2>Hearing:</h2>
+        <h3>
+            Assistive Listening Devices <br />
+            Loop Systems
+        </h3>
+        <h2>Closed Captioning</h2>
+        <h2>Audio Description</h2>
+        </div>
+        );
+}
+
+export default Accessibility;
